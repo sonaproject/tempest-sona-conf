e387384196d20a3a7bfb7fecadc67da0743002fb
Jian Li <pyguni@gmail.com>
2018-04-19 23:12:14 +0900

    Parameterize the keystone endpoint

--- admin-openrc.sh
+++ admin-openrc.sh
@@ -1,5 +1,5 @@
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=nova
-export OS_AUTH_URL=http://OPENSTACK_CONTROLLER_IP:35357/v2.0
+export OS_AUTH_URL=$KEYSTONE_ENDPOINT
 export LC_ALL=C
