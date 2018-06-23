6dcc47740ef953153aff271f5ff388ca6e0bac35
Jian Li <pyguni@gmail.com>
2018-06-24 02:34:19 +0900

    Start to support openstack pike release

--- admin-openrc.sh
+++ admin-openrc.sh
@@ -1,5 +1,10 @@
 export OS_TENANT_NAME=admin
 export OS_USERNAME=admin
 export OS_PASSWORD=nova
-export OS_AUTH_URL=$KEYSTONE_ENDPOINT
+export OS_DOMAIN=Default
+export OS_AUTH_URL=http://10.2.1.4/identity/v3/
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=admin
+
 export LC_ALL=C
